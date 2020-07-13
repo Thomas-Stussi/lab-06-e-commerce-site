6b059a8bb7ed37d58fe30b6d510275655ee38c03
Thomas Stussi <tstussi06@gmail.com>
2020-07-13 14:14:16 -0700

    Design list and passing test!

---
+++ renderCard.js
@@ -0,0 +1,30 @@
+export function renderCard(card) {
+    const li = document.createElement('li');
+    li.className = card.category;
+    li.title = card.description;
+
+    const h3 = document.createElement('h3');
+    h3.textContent = card.name;
+    li.appendChild(h3);
+
+    const img = document.createElement('img');
+    img.src = '../assets/' + card.image;
+    img.alt = card.name + ' image';
+    li.appendChild(img);
+
+    const p = document.createElement('p');
+    p.className = 'price';
+
+    const usd = '$' + card.price.toFixed(2);
+    
+    p.textContent = usd;
+    
+    const button = document.createElement('button');
+    button.textContent = 'Add';
+    button.value = card.id;
+    p.appendChild(button);
+
+    li.appendChild(p);
+
+    return li;
+}
--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,29 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
-
+import { renderCard } from '../renderCard.js';                         
 const test = QUnit.test;
+
 
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const shivanDragon = {
+        id: 'shivan',
+        name: 'Shivan Dragon',
+        image: 'shivan.jpg',
+        description: `While it's true most Dragons are cruel, the Shivan Dragon seems to take particular glee in the misery of others, often tormenting its victims much like a cat plays with a mouse before delivering the final blow.`,
+        category: 'Creature',
+        price: 6199.99,
+        color: 'red',
+        mana: 6
+    };
+
+    const expected = `<li class="Creature" title="While it's true most Dragons are cruel, the Shivan Dragon seems to take particular glee in the misery of others, often tormenting its victims much like a cat plays with a mouse before delivering the final blow."><h3>Shivan Dragon</h3><img src="../assets/shivan.jpg" alt="Shivan Dragon image"><p class="price">$6199.99<button value="shivan">Add</button></p></li>`;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const dom = renderCard(shivanDragon);
+    const actual = dom.outerHTML;
 
     //Expect
     // Make assertions about what is expected versus the actual result
