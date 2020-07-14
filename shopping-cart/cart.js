c810b280aa2cf29d2a8528b92ac60c60bfb735d5
Thomas Stussi <tstussi06@gmail.com>
2020-07-14 13:43:55 -0700

    dummy cart data

---
+++ shopping-cart/cart.js
@@ -0,0 +1,18 @@
+const cart = [{
+    id: 'moat',
+    quantity: 2
+}, {
+    id: 'shivan',
+    quantity: 1
+}, {
+    id: 'counter',
+    quantity: 4
+}, {
+    id: 'dark',
+    quantity: 3
+}, {
+    id: 'force',
+    quantity: 1
+}];
+
+export default cart;
